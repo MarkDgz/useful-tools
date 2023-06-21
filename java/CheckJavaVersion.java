26171557e6dbc559a36d853dfc6ba383d72781ea
Mark Dgz <tnemexico@gmail.com>
2023-06-20 23:36:38 -0300

    Create CheckJavaVersion.java
    Sample Class to Check Java Installed Version

---
+++ java/CheckJavaVersion.java
@@ -0,0 +1,24 @@
+/*
+How many times you need to check your java installed version?
+
+Don't worry you can download this free java class (https://upfiles.com/cgD3kek), 
+
+Run it and tada!...
+
+Samples: 
+
+(Linux) $ java CheckJavaVersion 10 
+
+(Windows) c:\java.exe CheckJavaVersion 10
+
+If it helps and want to earn some money with our refferal link: https://upfiles.com/ref/MKMan
+
+*/
+
+public class CheckJavaVersion {
+    public static void main(String[] args) {
+            System.out.println("Download class at this link https://upfiles.com/cgD3kek");
+        }
+    }
+}
+
